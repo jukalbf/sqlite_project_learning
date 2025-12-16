ea62836b8552e5ef377bc6fb6a50aba0dfdf42b9
jukalbf <lgbr212@gmail.com>
2025-12-16 01:03:27 -0300

    fix(personRoutes): Ajustando import errado

--- src/routes/personRoutes.js
+++ src/routes/personRoutes.js
@@ -1,10 +1,9 @@
 import PersonController from "../controllers/personController.js";
 import express from "express";
-import PersonModel from "../models/personModel.js";
 
 const router = express.Router();
 
-router.get("/", new PersonModel().getPeople);
+router.get("/", new PersonController().getPeople);
 router.post("/", new PersonController().createPerson);
 
 export default router;
