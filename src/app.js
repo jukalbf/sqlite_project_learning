7d6751156a9194e846f5c089ba9863448e09040d
jukalbf <lgbr212@gmail.com>
2025-12-16 01:02:38 -0300

    feat(app): Adicionando cors e express.json

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import express from "express";
 import personRouter from "./routes/personRoutes.js";
-import {Sequelize} from "sequelize";
+import cors from "cors";
 import tryConnection, {sequelize} from "../database/sequelizeConnection.js";
 
 const PORT = 8000;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.use("/people", personRouter);
 app.get("/", (req, res) => {
     return res.send(`Funcionando: http://locahost:${PORT}/people`);
 });
 
+tryConnection();
+
 app.listen(PORT, () => {
     console.log(`Rodando na porta ${PORT}: http://localhost:${PORT}/`);
-    tryConnection();
 });
